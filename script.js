307fd351b13005ea25a7c93004f81d04d21e31fe
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-05 16:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function stringChop(str, size) {
   // your code here
+  let ans=[], chunk="";
+  
+  for(let i=0;i<str.length;i++){
+      if(i!=0 && i%size == 0){
+          ans.push(chunk);
+          chunk = str.charAt(i);
+      }
+      
+      else{
+          chunk+=str.charAt(i);
+      }
+  }
+  
+  ans.push(chunk);
+  
+  return ans;
 }
 
 // Do not change the code below
